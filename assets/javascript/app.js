23f402220770cd897ee458d6a5db1681d461e7e0
adam fader <adamfader@gmail.com>
2016-05-30 17:05:22 -0400

    added barebones html and pseudo code in app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+//Initialize global variables
+
+
+$(document).ready(function(){
+
+
+
+});
+
+//organize questions and possible answers and answer videos in object
+
+//randomly select question
+
+//set timer for question
+
+//if answer is selected, check if it is correct or incorrect
+
+//record correct and incorrect guesses
+
+//if no answer is chosen and timer runs out, the correct answer is displayed
+
+//check to see if all questions have been asked and if game is over
+
+//automatically select a new question and restart the timer
+
+//if game is over, display the score and show button to restart the game
