aaf68e2241b084ffae5406282722ac1e19cd1d8b
adam fader <adamfader@gmail.com>
2016-05-31 17:50:06 -0400

    added basic game functions and minor html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,41 @@
 //Initialize global variables
-
+var pos = 0, trivia, triviaStatus, question, choice, choices, ch1, ch2, ch3, correct = 0, incorrect = 0;
+var questions = [
+	["What was the name of Homer's motorcycle gang?", "The Christ Punchers", "The Hell's Satans", "The Fat Boys", "2"],
+	["What is Principal Skinner's real name?", "Armin Tamzarian", "Skip Rhodes", "Hans Moleman", "1"],
+	["What was Lisa's nickname at school when she pretended to be a boy in order to attend the boys only classes?", "Skid Mark", "Toilet", "Brain Boy", "2"]
+];
 
 $(document).ready(function(){
 
+	$(document).on('click', '.new', function(){
+		showQuestion();
+	});
 
+	function showQuestion(){
+		$("#triviaStatus").html("Question "+(pos+1)+" of "+questions.length);
+		question = questions[pos][0];
+		ch1 = questions[pos][1];
+		ch2 = questions[pos][2];
+		ch3 = questions[pos][3];
+		$("#trivia").html("<h3>"+question+"</h3>");
+		$("#choice1").html(ch1);
+		$("#choice2").html(ch2);
+		$("#choice3").html(ch3);
+	};
+
+	$('.choices').on('click', function(){
+		guess = $(this).attr('value');
+		if(guess == questions[pos][4]){
+			correct++;
+		}else{
+			incorrect++;
+		}
+		pos++;
+		console.log(correct);
+		console.log(incorrect);
+		showQuestion();	
+	});
 
 });
 
