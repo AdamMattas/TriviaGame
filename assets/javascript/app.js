712277e1d5e432c9f0b30604919e8d8dc0cf419b
adam fader <adamfader@gmail.com>
2016-06-03 03:25:59 -0400

    completed game functions and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
+
 	//Initialize global variables
-	var vid1 = document.getElementById("video1");
+	var doh = $('#doh')[0];
+	var woohoo = $('#woohoo')[0];
+	var vid1 = document.getElementById("video2");
+	var vid2 = document.getElementById("video2");
+	var vid3 = document.getElementById("video2");
+	var vid4 = document.getElementById("video2");
+	var vid5 = document.getElementById("video2");
+	var vid6 = document.getElementById("video2");
+	var vid7 = document.getElementById("video2");
+	var vid8 = document.getElementById("video2");
+	var vid9 = document.getElementById("video2");
+	var vid10 = document.getElementById("video2");
 	//organize questions and possible answers and answer videos in object
-	var pos = 0, trivia, triviaStatus, question, choice, choices, ch1, ch2, ch3, correct = 0, incorrect = 0, counter;
+	var pos = 0, trivia, triviaStatus, question, choice, choices, ch1, ch2, ch3, counter;
+	var correct = 0;
+	var incorrect = 0;
 	var questions = [
 		["What was the name of Homer's motorcycle gang?", "The Christ Punchers", "The Hell's Satans", "The Fat Boys", "2", vid1],
 		["What is Principal Skinner's real name?", "Armin Tamzarian", "Skip Rhodes", "Hans Moleman", "1", vid2],
@@ -10,60 +23,83 @@ $(document).ready(function(){
 		['In the Treehouse of Horror episode "The Devil and Homer Simpson", Homer states that he would sell his soul for a doughnut. Which character appears as The Devil to seal the deal?', "Mr. Burns", "Sideshow Bob", "Ned Flanders", "3", vid4],
 		["When Bart was prescribed the drug Focusyn for his A.D.D., what kind of fruit did he put in his pants and say that his testicles wouldn't fit in his underwear?", "Oranges", "Apples", "Peaches", "1", vid5],
 		["When the Simpson family went to Florida during spring break, how many boobs did Homer tell Marge that he saw?", "15", "20", "12", "1", vid6],
-		["When Homer and Mel Gibson were on the run from film studio executives, what plan did Homer and Mel use to end the pursuit?", "Hold Mel Gibson hostage.", "Hide in the doughnut shop.", "Moon them until they could take no more.", "3", vid7],
+		["When Homer and Mel Gibson were on the run from film studio executives, what plan did Homer and Mel use to end the pursuit?", "Hold Mel Gibson hostage", "Hide in the doughnut shop", "Moon them until they could take no more", "3", vid7],
 		["When Lisa was babysitting Rod & Todd Flanders, what was the bedtime story she told them about?", "Anointing the feet of Jesus", "Imagination Christmas", "Two robots named Rod & Todd", "3", vid8],
 		["Where did Ralph Wiggum find a moon rock?", "On the moon", "In his nose", "In the backyard", "2", vid9],
-		[""]
+		["When Homer received counterfeit tickets to the Super Bowl, what did they turn out to be printed on?", "Toilet Paper", "Crackers", "Parking tickets", "2", vid10]
 	];
+$(document).ready(function(){
+	$('#questionBack').addClass('hide');
+	$(vid1).removeClass('hide');
+	vid1.play();
+	$(vid1).bind('ended', function() {
+		$(vid1).addClass('hide');
+		$('#img1').removeClass('hide');
+		$('#questionBack').removeClass('hide');
+	});
+	//game timer object functions
+	//set initial time and reset to initial time
 	var gameTimer = {
     time:30,
     reset: function(){
         gameTimer.time = 30;
         $('#display').html('30');
     },
+    //set interval at 1 second
     start: function(){
         counter = setInterval(gameTimer.count, 1000);
     },
+    //clear the counter variable
     stop: function(){
         clearInterval(counter);
     },
+    //decrease seconds and display them
     count: function(){
         gameTimer.time--;
         var converted = gameTimer.timeConverter(gameTimer.time);
         $('#display').html(converted);
     },
+    //check if time has expired and returns the time
     timeConverter: function(t){
         var seconds = t;
-        if (seconds <= 0){
-            skipQuestion();
+        if(seconds <= 0){
+        	skipQuestion();
         }
         return seconds;
     }
 	};
 
-	$(document).on('click', '.new', function(){
+	//checks for click to start a new game
+	$(document).on('click', '.start-button', function(){
 		showQuestion();
+		$('#img1').addClass('hide');
+		$('#start').fadeOut("slow");
+		$('#answers').animate({bottom: '0px'});
 	});
 
 	function showQuestion(){
 		//check if game is over and display the score and show button to restart the game
 		if(pos >= questions.length){
+			$('#answers').animate({bottom: '-300px'});
+			$('#start').fadeIn("slow");
 			$('#trivia').html("<h2>You got "+correct+" of "+questions.length+" questions correct</h2>");
-			$('#triviaStatus').html('Test Completed');
+			$('#triviaStatus').html('Simpsons Trivia Completed');
 			pos = 0;
 			correct = 0;
 			incorrect = 0;
 		}else{
+			//make sure timer is ready for a new game
 			gameTimer.stop();
 			gameTimer.reset();
 			gameTimer.start();
+			$('#answers').animate({bottom: '0px'});
 			//select question by the pos variable
 			$('#triviaStatus').html("Question "+(pos+1)+" of "+questions.length);
 			question = questions[pos][0];
 			ch1 = questions[pos][1];
 			ch2 = questions[pos][2];
 			ch3 = questions[pos][3];
-			$('#trivia').html("<h3>"+question+"</h3>");
+			$('#trivia').html("<h1>"+question+"</h1>");
 			$('#choice1').html(ch1);
 			$('#choice2').html(ch2);
 			$('#choice3').html(ch3);
@@ -71,11 +107,21 @@ $(document).ready(function(){
 	};
 
 	function skipQuestion(){
+		$('#answers').animate({bottom: '-300px'});
 		incorrect++;
+		$('#incorrect').html('<span class="correct-num">Incorrect &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + incorrect + '</span>');
+		$('#incorrect').animate({left: '-50px'});
+		$('#doh').get(0).play();
 		gameTimer.stop();
+		$('#questionBack').addClass('hide');
+		$(questions[pos][5]).removeClass('hide');
 		questions[pos][5].play();
-		questions[pos][5].addEventListener('ended', function(e) {
+		$(questions[pos][5]).bind('ended', function() {
+			$(questions[pos][5]).unbind('ended');
+			$(questions[pos][5]).addClass('hide');
+			$('#questionBack').removeClass('hide');
 			pos++;
+			$('#incorrect').animate({left: '-225px'});
 			showQuestion();
 		});
 
@@ -84,30 +130,46 @@ $(document).ready(function(){
 
 	//check if guess is correct and record correct and incorrect guesses
 	$('.choices').on('click', function(){
+		$('#answers').animate({bottom: '-300px'});
 		guess = $(this).attr('value');
 		if(guess == questions[pos][4]){
 			correct++;
+			$('#correct').html('<span class="correct-num">Correct &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + correct + '</span>');
+			$('#correct').animate({left: '-50px'});
+			$('#woohoo').get(0).play();
 			gameTimer.stop();
+			$('#questionBack').addClass('hide');
+			$(questions[pos][5]).removeClass('hide');
 			questions[pos][5].play();
-			questions[pos][5].addEventListener('ended', function(e) { 
+			$(questions[pos][5]).bind('ended', function() {
+				$(questions[pos][5]).unbind('ended');
+				$(questions[pos][5]).addClass('hide');
+				$('#questionBack').removeClass('hide');
+				pos++;
+				$('#correct').animate({left: '-225px'});
 				showQuestion();
 			});
 		}else{
 			incorrect++;
+			$('#incorrect').html('<span class="correct-num">Incorrect &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + incorrect + '</span>');
+			$('#incorrect').animate({left: '-50px'});
+			$('#doh').get(0).play();
 			gameTimer.stop();
+			$('#questionBack').addClass('hide');
+			$(questions[pos][5]).removeClass('hide');
 			questions[pos][5].play();
-			questions[pos][5].addEventListener('ended', function(e) { 
+			$(questions[pos][5]).bind('ended', function() {
+				$(questions[pos][5]).unbind('ended');
+				$(questions[pos][5]).addClass('hide');
+				$('#questionBack').removeClass('hide');
+				pos++;
+				$('#incorrect').animate({left: '-225px'});
 				showQuestion();
 			});
-		}
-		pos++;
-		console.log(correct);
-		console.log(incorrect);	
+		}	
 	});
 
 });
-
-//set timer for question
 
 //if no answer is chosen and timer runs out, the correct answer is displayed
 
